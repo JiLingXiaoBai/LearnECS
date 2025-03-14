a3290d42770d7fd4995725eac0cea1d68c567141
Xiao Bai <uiojkl3588@qq.com>
2025-03-14 17:47:23 +0800

    spawn bullets and zombies, random walking and health bar

--- Assets/Scripts/Authoring/HealthAuthoring.cs
+++ Assets/Scripts/Authoring/HealthAuthoring.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 public class HealthAuthoring : MonoBehaviour
 {
     public int healthAmount;
+    public int healthAmountMax;
 
     public class Baker : Baker<HealthAuthoring>
     {
         public override void Bake(HealthAuthoring authoring)
         {
             var entity = GetEntity(TransformUsageFlags.Dynamic);
-            AddComponent(entity, new Health { healthAmount = authoring.healthAmount });
+            AddComponent(entity, new Health
+            {
+                healthAmount = authoring.healthAmount,
+                healthAmountMax = authoring.healthAmountMax,
+                onHealthChanged = true,
+            });
         }
     }
 }
@@ -18,4 +24,6 @@ public class HealthAuthoring : MonoBehaviour
 public struct Health : IComponentData
 {
     public int healthAmount;
+    public int healthAmountMax;
+    public bool onHealthChanged;
 }
--- Assets/Scripts/Systems/ShootAttackSystem.cs
+++ Assets/Scripts/Systems/ShootAttackSystem.cs
@@ -1,27 +1,63 @@
 using Unity.Burst;
 using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
 
 partial struct ShootAttackSystem : ISystem
 {
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        foreach ((RefRW<ShootAttack> shootAttack, RefRO<Target> target) in SystemAPI
-                     .Query<RefRW<ShootAttack>, RefRO<Target>>())
+        EntitiesReferences entitiesReferences = SystemAPI.GetSingleton<EntitiesReferences>();
+
+        foreach ((RefRW<LocalTransform> localTransform, RefRW<ShootAttack> shootAttack,
+                     RefRO<Target> target, RefRW<UnitMover> unitMover) in SystemAPI
+                     .Query<RefRW<LocalTransform>, RefRW<ShootAttack>, RefRO<Target>, RefRW<UnitMover>>())
         {
             if (target.ValueRO.targetEntity == Entity.Null)
             {
                 continue;
             }
+
+            LocalTransform targetLocalTransform = SystemAPI.GetComponent<LocalTransform>(target.ValueRO.targetEntity);
+
+            if (math.distancesq(localTransform.ValueRO.Position, targetLocalTransform.Position) >
+                math.pow(shootAttack.ValueRO.attackDistance, 2))
+            {
+                //Not close enough, start moving towards target
+                unitMover.ValueRW.targetPosition = targetLocalTransform.Position;
+                continue;
+            }
+            else
+            {
+                //Close enough, stop moving and attack
+                unitMover.ValueRW.targetPosition = localTransform.ValueRO.Position;
+            }
+
+            float3 aimDirection = targetLocalTransform.Position - localTransform.ValueRO.Position;
+            aimDirection = math.normalize(aimDirection);
+            quaternion targetRotation = quaternion.LookRotation(aimDirection, math.up());
+            localTransform.ValueRW.Rotation = math.slerp(localTransform.ValueRO.Rotation, targetRotation,
+                unitMover.ValueRO.rotationSpeed * SystemAPI.Time.DeltaTime);
+
             shootAttack.ValueRW.timer -= SystemAPI.Time.DeltaTime;
             if (shootAttack.ValueRW.timer > 0)
             {
                 continue;
             }
             shootAttack.ValueRW.timer = shootAttack.ValueRW.timerMax;
-            RefRW<Health> targetHealth = SystemAPI.GetComponentRW<Health>(target.ValueRO.targetEntity);
-            int damageAmount = 1;
-            targetHealth.ValueRW.healthAmount -= damageAmount;
+
+            Entity bulletEntity = state.EntityManager.Instantiate(entitiesReferences.bulletPrefabEntity);
+            float3 bulletSpawnWorldPosition =
+                localTransform.ValueRO.TransformPoint(shootAttack.ValueRO.bulletSpawnLocalPosition);
+            
+            SystemAPI.SetComponent(bulletEntity, LocalTransform.FromPosition(bulletSpawnWorldPosition));
+
+            RefRW<Bullet> bulletBullet = SystemAPI.GetComponentRW<Bullet>(bulletEntity);
+            bulletBullet.ValueRW.damageAmount = shootAttack.ValueRO.damageAmount;
+
+            RefRW<Target> bulletTarget = SystemAPI.GetComponentRW<Target>(bulletEntity);
+            bulletTarget.ValueRW.targetEntity = target.ValueRO.targetEntity;
         }
     }
 }
