02db70dc543023ae22d8699cb79e5741d1e69074
Xiao Bai <uiojkl3588@qq.com>
2025-03-27 15:00:17 +0800

    fog of war

--- Assets/Scripts/MonoBehaviours/GameAssets.cs
+++ Assets/Scripts/MonoBehaviours/GameAssets.cs
@@ -6,6 +6,7 @@ public class GameAssets : MonoBehaviour
     public const int BUILDINGS_LAYER = 7;
     public const int PATHFINDING_WALLS = 8;
     public const int PATHFINDING_HEAVY = 9;
+    public const int FOG_OF_WAR = 11;
     public static GameAssets Instance { get; private set; }
 
     private void Awake()
